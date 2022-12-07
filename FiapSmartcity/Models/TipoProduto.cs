b2f70a19dc418902591b405bc58dc33c500fb6e3
Camis <Camis@MacBook-Pro.local>
2022-12-06 22:36:50 -0300

    :art: fiapsmartcity

---
+++ FiapSmartcity/Models/TipoProduto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FiapSmartcity.Models
+{
+	public class TipoProduto
+	{
+		
+        public int IdTipo { get; set; }
+        public String DescricaoTipo { get; set; }
+        public bool Comercializado { get; set; }
+        
+    }
+}
